e95837d8df2d2974db302a2d5da938900c369d42
laxmikant02 <dadagilaxmikant02@gmail.com>
2025-08-03 19:41:53 +0530

    rename SCR_ML_01 to SCT_ML_1

--- SCT_ML_1/wsgi.py
+++ SCT_ML_1/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SCR_ML_01.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SCT_ML_1.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SCR_ML_01.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SCT_ML_1.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
